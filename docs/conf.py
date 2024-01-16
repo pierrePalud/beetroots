44d50c97e4510d8c25c9ce9b32304620d46d5735
ppalud <pierre.palud@centralille.fr>
2024-01-16 16:12:47 +0100

    read the docs (3)

--- docs/conf.py
+++ docs/conf.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.abspath("../"))
 project = "beetroots"
 copyright = "2024, Pierre Palud"
 author = "Pierre Palud"
-release = "0.1.0"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
